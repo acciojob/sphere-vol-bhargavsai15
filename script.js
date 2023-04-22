5c1f9c73654537482881758c6fb9df8938a89800
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 19:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
 function volume_sphere() {
-    //Write your code here
-  
+  //Write your code here
+	const radius = document.getElementById('radius');
+	const volume = document.getElementById('volume');
+	let result = 0;
+	volume.value = '';
+	const PI=3.141
+	  const r = parseInt(radius.value);
+	  result = 4 / 3 * PI * (r * r * r);
+	  volume.value = result;
+	  return result;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+	window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
