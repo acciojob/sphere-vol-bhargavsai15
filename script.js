73bb95c557e140dca360de063cc43caf8793d807
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 22:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const btn = document.getElementById('submit');
-btn.addEventListener('click', volume_sphere);
+
 function volume_sphere() {
-  const radius = parseFloat(document.getElementById('radius').value);
-  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById('volume').value = volume.toFixed(2);
+ event.preventDefault();
+    //Write your code here
+	const radius = document.getElementById("radius").value
+	let formula =(4/3)*Math.PI*(radius*radius*radius);
+	document.getElementById("volume").value = formula.toFixed(2);
 }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
